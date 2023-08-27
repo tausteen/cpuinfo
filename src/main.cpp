4533cb6e6c7e8ef35aa91e67a8b0a060760d77ea
Thor Audun Steen <tasn@datarespons.no>
2023-08-27 13:01:22 +0200

    Remove frequency as own row

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,10 @@ int main(int argc, char **argv)
 {
     hwinfo::CPU cpu;
     std::cout << "--------------------------------- CPU INFO ---------------------------------" << std::endl;
-    std::cout << std::left << std::setw(20) << "model:";
+    std::cout << std::left << std::setw(25) << "model and frequency:";
     std::cout << cpu.modelName() << std::endl;
-    std::cout << std::left << std::setw(20) << "physical cores:";
+    std::cout << std::left << std::setw(25) << "physical cores:";
     std::cout << cpu.numPhysicalCores() << std::endl;
-    std::cout << std::left << std::setw(20) << "logical cores:";
+    std::cout << std::left << std::setw(25) << "logical cores:";
     std::cout << cpu.numLogicalCores() << std::endl;
-    std::cout << std::left << std::setw(20) << "max frequency:";
-    std::cout << cpu.maxClockSpeed_kHz() << std::endl;
-    std::cout << std::left << std::setw(20) << "regular frequency:";
-    std::cout << cpu.regularClockSpeed_kHz() << std::endl;
-    std::cout << std::left << std::setw(20) << "current frequency:";
-    std::cout << hwinfo::CPU::currentClockSpeed_kHz() << std::endl;
 }
